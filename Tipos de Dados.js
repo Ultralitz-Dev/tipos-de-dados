3bed4207acfed02fc6186792afc244f72e4530f9
João Vitor <129129458+Ultralitz-Dev@users.noreply.github.com>
2023-04-03 21:05:09 -0300

    Add files via upload

---
+++ Tipos de Dados.js
@@ -0,0 +1,11 @@
+var lampada_ligada = false; 
+var maior_idade = true; 
+
+var idade = 21; 
+var temperatura = -5; 
+
+var saldo = 5259.32; 
+var valor = -25.00; 
+
+var nome = "Felipe";
+var empresa = 'Microsoft';
